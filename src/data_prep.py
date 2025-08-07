587406836febf1f7a51f17222e1c52680ad5c0d9
2shR <tusharrastogi0422@gmail.com>
2025-08-07 10:38:45 +0530

    Create data_prep.py

---
+++ src/data_prep.py
@@ -0,0 +1,30 @@
+# src/data_prep.py
+
+import pandas as pd
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+
+def load_data():
+    housing = fetch_california_housing(as_frame=True)
+    df = housing.frame
+    return df
+
+def preprocess_data(df):
+    X = df.drop("MedHouseVal", axis=1)
+    y = df["MedHouseVal"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    return X_train, X_test, y_train, y_test
+
+def save_datasets(X_train, X_test, y_train, y_test):
+    X_train.to_csv("data/processed/X_train.csv", index=False)
+    X_test.to_csv("data/processed/X_test.csv", index=False)
+    y_train.to_csv("data/processed/y_train.csv", index=False)
+    y_test.to_csv("data/processed/y_test.csv", index=False)
+
+if __name__ == "__main__":
+    df = load_data()
+    df.to_csv("data/raw/california_housing.csv", index=False)
+    
+    X_train, X_test, y_train, y_test = preprocess_data(df)
+    save_datasets(X_train, X_test, y_train, y_test)
