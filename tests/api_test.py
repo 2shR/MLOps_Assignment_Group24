c0ef637b496798ebb827b2cc4db2acac03e2af31
Tushar Rastogi <tusharrastogi0422@gmail.com>
2025-08-10 16:57:37 +0530

    run test issue fix

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,9 +1,6 @@
 import pytest
-from unittest.mock import patch
+from src.api import app
 
-# Patch MlflowClient and pyfunc.load_model before importing app
-with patch("mlflow.tracking.MlflowClient"), patch("mlflow.pyfunc.load_model"):
-    from src.api import app
 
 @pytest.fixture
 def client():
@@ -11,13 +8,16 @@ def client():
     with app.test_client() as client:
         yield client
 
+
 def test_home(client):
     response = client.get("/")
     assert response.status_code == 200
 
+
 def test_predict_post_200(client):
     response = client.post("/predict", json={ "MedInc": 8.3252, "HouseAge": 41.0, "AveRooms": 6.9841, "AveBedrms": 1.0238, "Population": 322.0, "AveOccup": 2.5556, "Latitude": 37.88, "Longitude": -122.23})
     assert response.status_code == 200 
+
 
 def test_predict_post_Error(client):
     response = client.post("/predict", json={ "test":1.0})
