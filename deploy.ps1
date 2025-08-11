994bf08ad93bb7f12242c4290a4b4b31dcdecfd7
Tushar Rastogi <tusharrastogi0422@gmail.com>
2025-08-11 17:03:39 +0530

    DVC files commit for demo

--- deploy.ps1
+++ deploy.ps1
@@ -5,7 +5,7 @@ docker rm -f housing-api 2>$null
 # Pull latest image from Docker Hub
 docker pull tusharrastogi/housing-api:latest
 
-# Run new container
+# Run a new container
 docker run -d -p 8081:8081 --name housing-api tusharrastogi/housing-api:latest
 
 Write-Host "Deployment complete! API should be accessible at http://localhost:8081/predict"
