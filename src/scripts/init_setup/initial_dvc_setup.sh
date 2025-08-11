2b0146a260f64e74b090d55e4b8b9bb51ebe9ffa
Tushar Rastogi <tusharrastogi0422@gmail.com>
2025-08-11 16:26:22 +0530

    dvc files removed for demo

--- src/scripts/init_setup/initial_dvc_setup.sh
+++ src/scripts/init_setup/initial_dvc_setup.sh
@@ -1,4 +1,11 @@
 pip install dvc pandas scikit-learn matplotlib jupyter
-dvc init
+#For the first time, initialize DVC
+dvc init -f
+# Add the data directory to DVC
+dvc add data/raw/california_housing.csv
+dvc add data/processed/X_train.csv
+dvc add data/processed/X_test.csv
+dvc add data/processed/y_train.csv
+dvc add data/processed/y_test.csv
 git add data/*.dvc
 git commit -m "Initial commit with DVC"
